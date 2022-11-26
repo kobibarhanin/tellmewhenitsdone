3fdaf99ef2eb8663638697d13ff6cb0219be5335
Kobi Bar Hanin <kobibarhanin@gmail.com>
2022-11-26 09:24:19 +0200

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 
+const SECOND = 1000;
+
 const sleep = async (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
 app.get('/:interval', async (req, res) => {
+  const interval = req.params.interval;
+  if (interval > 30 * SECOND) {
+    res.status(400).send('Interval too long');
+    return;
+  }
+
   await sleep(req.params.interval);
   res.json({ answer: 42 });
 });
